d28fa0511154da546e370038d2838502faebdb2e
Angel <albsabater.bit@gmail.com>
2019-07-31 23:25:30 +0900

    up & down for loop fixes

--- components/Game.js
+++ components/Game.js
@@ -142,7 +142,7 @@ function flipableCells(id, currentPlayer, G) {
   tempCells = [];
 
   // flip up direction
-  for (i = id - 5; i < 0; i-=5) {
+  for (i = id - 5; i >= 0; i-=5) {
     if (G.cells[i].piece === null) {
       tempCells = [];
       break;
@@ -159,7 +159,7 @@ function flipableCells(id, currentPlayer, G) {
   tempCells = [];
 
   // flip down direction
-  for (i = id + 5; i > 24; i += 5) {
+  for (i = id + 5; i <= 24; i += 5) {
     if (G.cells[i].piece === null) {
       tempCells = [];
       break;
