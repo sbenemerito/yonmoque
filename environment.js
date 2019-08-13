5e44e962e268ceb63df685cfd203fc0b8a735498
Sam Benemerito <benemeritosam@gmail.com>
2019-08-13 22:38:08 +0900

    add base environment file, new dependencies

---
+++ environment.js
@@ -0,0 +1,33 @@
+import Constants from 'expo-constants'
+
+
+const ENV = {
+ dev: {
+   apiUrl: 'http://192.168.0.131:5000',
+   amplitudeApiKey: null,
+ },
+ staging: {
+   apiUrl: "[your.staging.api.here]",
+   amplitudeApiKey: "[Enter your key here]",
+   // Other keys
+ },
+ prod: {
+   apiUrl: "[your.production.api.here]",
+   amplitudeApiKey: "[Enter your key here]",
+   // Other keys
+ }
+};
+
+const getEnvVars = (env = Constants.manifest.releaseChannel) => {
+ // The __DEV__ variable is set to true when react-native is in Dev mode.
+ // __DEV__ is true when run locally, but false when published.
+ if (__DEV__) {
+   return ENV.dev;
+ } else if (env === 'staging') {
+   return ENV.staging;
+ } else if (env === 'prod') {
+   return ENV.prod;
+ }
+};
+
+export default getEnvVars;
